bcb3586729d0afb4efe5d2104e3342de88be784e
asw11wsa <asw11wsa@naver.com>
2021-10-06 21:22:17 +0900

    protected secret information

--- Models/User.js
+++ Models/User.js
@@ -33,4 +33,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-module.exports - { User }
+module.exports = { User }
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const config = require('./config/key');
+const { User } = require('./Models/User');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://asw11wsa:tjdghk5328@cluster0.jhklw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true,useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log(err))
@@ -11,7 +17,21 @@ mongoose.connect('mongodb+srv://asw11wsa:tjdghk5328@cluster0.jhklw.mongodb.net/m
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요')
+})
+
+app.post('/register',(req, res) => {
+  //회원가입 할때 필요한 정보들을 클라이언트에서 가져오면 
+  //그것들을 데이터 베이스에 넣어 준다.
+
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
